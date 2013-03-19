2e5c6b3edfebd164d5f0d39d92becdd5b24b0d1c
Andrew McCaskey <andy.mccaskey@gmail.com>
2013-03-19 16:04:10 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nadrails::Application.config.secret_token = '7f7d0c59610ccb74f68576244a2d9449f0582e40d931ebb86916bde8cf00f6eec19e00d7660f42170fe697f5fafa2745c5fe5702ac27f5ecb767d12810adfc4c'
