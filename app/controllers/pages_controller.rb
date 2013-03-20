a6ee9161f06a6e5eb439490b8176d49697b0c0b4
Andrew McCaskey <andy.mccaskey@gmail.com>
2013-03-20 15:59:29 -0400

    Setup Pages Controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def bidlist 
+  end
+
+  def story
+  end
+
+  def account
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Nadrails::Application.routes.draw do
+  root :to => 'pages#home'
+  get 'bidlist' => 'pages#bidlist'
+  get 'account' => 'pages#account'
+  get 'story' => 'pages#story'
+
+end
+
+  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +64,4 @@ Nadrails::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
